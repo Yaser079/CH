43ea39ca846156eff61cbda332164addea5f1f2f
up2asad <yasermushtaq655@gmail.com>
2023-10-23 07:14:27 +0500

    login functionality and theme layout

---
+++ Admin/index.php
@@ -0,0 +1,50 @@
+<?php session_start(); include '../script/islogin.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title><?= $_SESSION['site']?> | Dashboard</title>
+  <?php include '../Inc/head.php';?>
+
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  
+  <!-- /.navbar -->
+  <?php include '../Inc/top-nav.php';?>
+  <!-- Main Sidebar Container -->
+  <?php include '../Inc/side-bar.php';?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="text-dark">Dashboard</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+       
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php include '../Inc/footer.php';?>
+</body>
+</html>
